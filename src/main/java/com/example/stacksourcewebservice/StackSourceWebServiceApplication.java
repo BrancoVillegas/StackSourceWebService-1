a07686e5e717b56b7508b919ee4b66b7c058f50d
Abel Cierto <u20201b561@upc.edu.pe>
2023-05-03 11:35:19 -0500

    Initial commit

---
+++ src/main/java/com/example/stacksourcewebservice/StackSourceWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stacksourcewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackSourceWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackSourceWebServiceApplication.class, args);
+    }
+
+}
